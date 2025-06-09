0c7982e7ff07eee8aef2aa3dd5919b14fb3e6d56
Pavan Kumar <gpavankumar942@gmail.com>
2025-06-09 19:44:18 +0530

    fixed images

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ async function searchImages(){
         const imageWrapper = document.createElement ('div');
         imageWrapper.classList.add("search-result");
         const image = document.createElement("img");
-        image.src = result.urls.samll;
+        image.src = result.urls.small;
         image.alt = result.alt_description;
         const imageLink = document.createElement('a');
         imageLink.href = result.links.html;
